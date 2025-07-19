b892a7153ffdeb1e6ee08fb3be4d586bc1d95ab1
Jay7751 <jogijayanth28@gmail.com>
2025-07-19 22:03:19 +0530

    script commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const taskInput = document.getElementById("taskInput")
+const taskList = document.getElementById("taskList")
+function addTask(){
+    const task = taskInput.value.trim();
+    if(task=="") return;
+    const li = document.createElement("li");
+    li.testContent = task;
+    li.addEventListener("click",()=>{
+        li.remove();
+    });
+    taskList.appendChild(li);
+    taskInput.value = "";
+}
